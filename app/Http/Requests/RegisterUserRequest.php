bb535ba34a90eb0c37f3b4d6b812daa823ea9b40
samer <sam.ayash200@gmail.com>
2019-10-03 16:24:08 +0300

    Initial commit

---
+++ app/Http/Requests/RegisterUserRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Post;
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $request = request();
+
+        $user = auth()->user();
+
+        return Post::find($request->post_id)->user_id == auth()->id();
+
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+        ];
+    }
+}
